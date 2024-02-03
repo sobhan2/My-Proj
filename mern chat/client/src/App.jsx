99c00f73deaf7222294cf17efe9d8655d718595b
sobhan2 <78255099+sobhan2@users.noreply.github.com>
2024-02-03 17:32:07 +0000

    1

---
+++ mern chat/client/src/App.jsx
@@ -0,0 +1,11 @@
+import Register from './register'
+
+
+function App() {
+  
+  return (
+   <Register/>
+  )
+}
+
+export default App
---
+++ mern chat/client/src/register.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Register() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    return(
+        <div className="bg-blue-50 h-screen flex items-center">
+            <form action="" className="w-64 mx-auto mb-12" >
+                <input value={username}
+                    onChange={ev => setUsername(ev.target.value)}
+                    type="text" placeholder="username"
+                    className="block rounded-md p-2 mb-2 w-full border" />
+                <input value={password}
+                    onChange={ev => setPassword(ev.target.value)} 
+                    type="password" placeholder="password"
+                    className="block w-full rounded-md p-2 mb-2 border" />
+                <button className="bg-blue-500 text-white block w-full rounded-md p-2 border" >Register</button>
+            </form>
+        </div>
+    );
+
+}
