6fa0b605dcb520cddf7be9397756ef9b81d44bdf
sobhan2 <78255099+sobhan2@users.noreply.github.com>
2024-02-11 17:54:44 +0000

    1

--- Mern/client/src/components/account/LoginDialog.jsx
+++ Mern/client/src/components/account/LoginDialog.jsx
@@ -1,18 +1,35 @@
 
 
-import {Dialog} from '@mui/material'
+import {Dialog , Box,List ,ListItem} from '@mui/material'
 
 const dialogStyle = {
     height: '95%',
-    width: '1160px',
-    margin
+    marginTop: '12%',
+    textAlign: 'center',
+    maxHeight: '100%',
+    maxWidth: '100%',
+    boxShadow: 'none',
+    overflow: 'none'
 }
 
 const LoginDialog = () => {
 
     return(
         <Dialog open={true} PaperProps={{sx: dialogStyle}}>
-            Hello
+            <h2>Welcome to FireDWeller</h2>
+            <Box>
+                <Box>
+                    <List>
+                        <ListItem>1. Open FireDWeller on your web server</ListItem>
+                        <ListItem>2. Tap</ListItem>
+                    </List>
+                </Box>
+                <Box>
+
+                </Box>
+            </Box>
+
+
         </Dialog>
     )
 }
