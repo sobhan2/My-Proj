a410cb753a2cabd72a3169c705aa075b6815bf15
sobhan2 <sobhanhero2001@gmail.com>
2024-02-19 07:44:21 +0530

    1

--- Mern/client/src/components/account/LoginDialog.jsx
+++ Mern/client/src/components/account/LoginDialog.jsx
@@ -1,6 +1,7 @@
 
 
-import {Dialog , Box,List ,ListItem} from '@mui/material'
+import {Dialog , Box,List ,ListItem, styled} from '@mui/material'
+import { qrCodeImage } from '../../constants/data';
 
 const dialogStyle = {
     height: '95%',
@@ -12,22 +13,30 @@ const dialogStyle = {
     overflow: 'none'
 }
 
+const Component = styled(Box)`
+    display: flex;
+`
+
+const Container = styled(Box)`
+    padding: 56px 0 56px 56px;
+`
+
 const LoginDialog = () => {
 
     return(
         <Dialog open={true} PaperProps={{sx: dialogStyle}}>
             <h2>Welcome to FireDWeller</h2>
-            <Box>
-                <Box>
+            <Component>
+                <Container>
                     <List>
                         <ListItem>1. Open FireDWeller on your web server</ListItem>
-                        <ListItem>2. Tap</ListItem>
+                        <ListItem>2. Tap Menu Settings and select FireDWeller</ListItem>
                     </List>
-                </Box>
+                </Container>
                 <Box>
-
+                    <img src={qrCodeImage} alt="qr code" />
                 </Box>
-            </Box>
+            </Component>
 
 
         </Dialog>
