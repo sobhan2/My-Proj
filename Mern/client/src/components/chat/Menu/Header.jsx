fe5e9561aa10e0ef00ab7c05cad0bcb5a4d1a7cd
sobhan2 <sobhanhero2001@gmail.com>
2024-02-22 13:14:08 +0530

    1

--- Mern/client/src/components/chat/Menu/Header.jsx
+++ Mern/client/src/components/chat/Menu/Header.jsx
@@ -5,19 +5,20 @@ import { Box, styled } from "@mui/material";
 
 
 const Component = styled(Box)`
-    height: 50px;
-    padding: 8px 16x;
-    background: grey;
-    margin: 0 0 0 10px;
-    border-radius: 5px 5px 0 0;
-    justifyContent: start;
+height: 50px;
+padding: 16px 16px;
+background: grey;
+margin: 0 0 0 10px;
+border-radius: 5px 5px 0 0;
+display: flex;
+align-items: center;
     
 `
 const Image = styled('img')({
     height: 40,
-    width: 40,
-    borderRadius: '50%',
-    
+  width: 40,
+  borderRadius: '50%',
+  marginLeft: "16px",
     
 })
 
@@ -27,7 +28,8 @@ const Header = () => {
 
     return ( 
         <>
-            <Component>
+            <Component sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <span>{account.name}</span>
                 <Image src={account.picture} alt="dp" />
             </Component>
         </>
